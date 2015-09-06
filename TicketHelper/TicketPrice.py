beff8cd575931f26bedb7670f369eaebd39ddfeb
forblackking <forblackking@gmail.com>
2015-09-06 19:54:20 +0800

    完成价格的输出,增加计时器
    下一步将着重完成TODO的内容并重构代码

--- TicketHelper/TicketPrice.py
+++ TicketHelper/TicketPrice.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: gbk -*
-'''
+
 import urllib
 import ssl
 import json
+import string
 
 def TicketPrice(train_no,from_station_no,to_station_no,seat_types,train_date):
     testurl='https://kyfw.12306.cn/otn/leftTicket/queryTicketPrice?train_no='+train_no+'&from_station_no='+from_station_no+'&to_station_no='+to_station_no+'&seat_types='+seat_types+'&train_date='+train_date
+    #print(testurl)
 
-    print(testurl)
-    pass
+    #证书问题
+    ssl._create_default_https_context = ssl._create_unverified_context
+    information = urllib.request.urlopen(str(testurl)).read().decode('gbk')
+    #print(information)
+    #END
+    information=str(information)
 
+    #print(information.replace('楼',''))
+    ticketcontent = json.loads(information.replace('楼',''))
 
-
-
-#证书问题
-ssl._create_default_https_context = ssl._create_unverified_context
-information = urllib.request.urlopen(str(testurl)).read().decode('utf-8')
-'''
+    return ticketcontent
--- TicketHelper/init.py
+++ TicketHelper/init.py
@@ -3,19 +3,23 @@
 
 import TicketHelper.connect
 import TicketHelper.Station
-#import TicketHelper.TicketPrice
+import TicketHelper.TicketPrice
 import urllib
 import ssl
 import json
+import time
 import sys
 import http  #.cookiejar
 
 
 #判断网络环境
 if(not(TicketHelper.connect.connect())):
-    #print("No network Connection.")
+    print("No network Connection.")
     exit()
 else:
+    #计时
+    time_start=time.time()
+    count=1;
     #获取站点&日期
     from_station = TicketHelper.Station.StartStation()
     to_station = TicketHelper.Station.EndStation()
@@ -62,18 +66,41 @@ else:
             if(item["wz_num"]=='无'):
                 item["wz_num"]='N'
 
+            start_train_date=str(item["start_train_date"])
+            start_train_date=start_train_date[:4]+'-'+start_train_date[4:6]+'-'+start_train_date[6:]
+            #print(start_train_date)
 
-          #  TicketHelper.TicketPrice.TicketPrice(item["train_no"],item["from_station_no"])
+            ticketcontent=TicketHelper.TicketPrice.TicketPrice(item["train_no"],item["from_station_no"],item["to_station_no"],item["seat_types"],start_train_date)
+            #print(ticketcontent)
+            price=ticketcontent['data']
+            #
+            if('M' in price):
+                M_price=str(price["M"])
+            else:
+                M_price='None'
 
+            if('O' in price):
+                O_price=str(price["O"])
+            else:
+                O_price='None'
 
-            print("车次",'%-5s' % item["station_train_code"],"出发时间",item["start_time"],"历时",item["lishi"],"一等座",'%-3s' % item["zy_num"],"价格","二等座",'%-3s' % item["ze_num"],
-                  "价格","无座",'%-3s' % item["wz_num"],"价格")
+            if('WZ' in price):
+                WZ_price=str(price["WZ"])
+            else:
+                WZh_price='None'
+            #
+
+            print("车次",'%-5s' % item["station_train_code"],"出发时间",item["start_time"],"到达时间",item["arrive_time"],"一等座",'%-3s' % item["zy_num"],'%-6s' % M_price,"二等座",'%-3s' % item["ze_num"],
+                  '%-6s' % O_price,"无座",'%-3s' % item["wz_num"],'%-6s' % WZ_price)
             #print(item)
+            count=count+1
 
     else:
         print("指定日期没有可搭乘的火车")
         pass
 
+    time_end=time.time()
+    print("搜索共耗时",time_end-time_start,"秒,获得",count,"条结果")
     #过滤结果
     ##过滤 ->"message":"没有符合条件的数据！"
 
